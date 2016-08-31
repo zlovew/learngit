f6517c5b28c0c9cf0c5678bca0c9139641408967
死不得 <850518603@qq.com>
2016-08-31 14:01:35 +0800

    继续完善

---
+++ myshop/app/src/main/java/qainfeng/myshop/adapter/HomeAdapter.java
@@ -0,0 +1,31 @@
+package qainfeng.myshop.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Administrator on 16-8-31.
+ */
+public class HomeAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
